d8b8bd548fa710b27d7ac77f8ca3b8bc976502cf
Phawit Pornwattanakul <me@helloyeew.dev>
2022-02-06 15:05:21 +0700

    Finish get and post method

--- main.py
+++ main.py
@@ -1,41 +1,52 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pymongo import MongoClient
 from pydantic import BaseModel
+from fastapi.encoders import jsonable_encoder
+
 
 class Reservation(BaseModel):
-    name : str
+    name: str
     time: int
     table_number: int
-    
+
+
 client = MongoClient('mongodb://localhost', 27017)
 
-# TODO fill in database name
-db = client["<put your database name>"]
+db = client["restaurant"]
 
-# TODO fill in collection name
-collection = db["<put your collection name>"]
+collection = db["reservation"]
 
 app = FastAPI()
 
 
-# TODO complete all endpoint.
 @app.get("/reservation/by-name/{name}")
-def get_reservation_by_name(name:str):
-    pass
+def get_reservation_by_name(name: str):
+    search_result = collection.find_one({"name": name}, {"_id": 0})
+    if search_result is None:
+        raise HTTPException(status_code=404, detail="Reservation not found")
+    return search_result
+
 
 @app.get("reservation/by-table/{table}")
 def get_reservation_by_table(table: int):
-    pass
+    search_result = collection.find_one({"table_number": table}, {"_id": 0})
+    if search_result is None:
+        raise HTTPException(status_code=404, detail="Reservation not found")
+    return search_result
+
 
 @app.post("/reservation")
-def reserve(reservation : Reservation):
-    pass
+def reserve(reservation: Reservation):
+    p = jsonable_encoder(reservation)
+    collection.insert_one(p)
+    # return full details of the reservation
+    return {"status": "ok"}
 
 @app.put("/reservation/update/")
 def update_reservation(reservation: Reservation):
-    pass
+    
+
 
 @app.delete("/reservation/delete/{name}/{table_number}")
-def cancel_reservation(name: str, table_number : int):
+def cancel_reservation(name: str, table_number: int):
     pass
-
